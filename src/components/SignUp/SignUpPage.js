b05c3b0ab3d69fad028ea959b0271367eb63802b
Charles Duong <charlesduong14@gmail.com>
2020-06-24 22:37:17 -0500

    Initial pass at sign up page

--- src/components/SignUp/SignUpPage.js
+++ src/components/SignUp/SignUpPage.js
@@ -1,8 +1,36 @@
 import React from 'react'
 import { PageLayout } from '../PageLayout'
+import Form from 'carbon-components-react/lib/components/Form'
+import FormGroup from 'carbon-components-react/lib/components/FormGroup'
+import TextInput from 'carbon-components-react/lib/components/TextInput'
 
 export const SignUpPage = () => (
   <PageLayout title="Sign Up" submitButtonTitle="Continue">
-
+    <Form style={{ width: "100%" }}>
+      <FormGroup>
+        <TextInput
+          id="first_name"
+          invalidText="Invalid error message."
+          labelText="First Name"
+          placeholder="Joan"
+        />
+      </FormGroup>
+      <FormGroup>
+        <TextInput
+          id="last_name"
+          invalidText="Invalid error message."
+          labelText="Last Name"
+          placeholder="Ambassador"
+        />
+      </FormGroup>
+      <FormGroup>
+        <TextInput
+          id="last_name"
+          invalidText="Invalid error message."
+          labelText="Date of Birth"
+          placeholder="mm/dd/yyyy"
+        />
+      </FormGroup>
+    </Form>
   </PageLayout>
 )
