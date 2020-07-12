2f03ac2fabf1c2e33122eca0604c6750432f68f5
Charles Duong <charlesduong14@gmail.com>
2020-07-12 12:24:03 -0500

    Fix floating action button on page layout

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -1,38 +1,36 @@
 import React from 'react'
-import { Grid, Column, Row, Form } from 'carbon-components-react'
+import { Grid, Column, Row, Form, Content } from 'carbon-components-react'
 import styled from 'styled-components'
 import { spacing, colors } from '../theme'
 import Menu from './Menu'
 import Button from './Button'
 import { InlineNotification } from 'carbon-components-react'
 
-const ContentContainer = styled(Grid)`
+const ContentContainer = styled(Content)`
   padding: ${ spacing[3] };
-  padding-top: ${ spacing[8] };
   padding-bottom: ${ spacing[10] };
-  height: 100vh;
-  margin-top: 0;
-  overflow-y: scroll;
 `
 
 const TitleContainer = styled(Row)`
-  margin-top: ${ spacing[7] };
+  margin-top: ${ spacing[5] };
   margin-bottom: ${ spacing[7] };
 `
 
 const CtaButtonContainer = styled(Column)`
-  position: absolute;
+  position: fixed;
   bottom: 0;
   left: 0;
-  background-color: ${ colors.white };
   z-index: 1;
 `
 
-const CtaButton = styled(Button)`
+const InlineNotificationStyled = styled(InlineNotification)`
   width: 100%;
   max-width: 100%;
-  margin-top: 0;
   margin-bottom: ${ spacing[3] };
+`
+
+const CtaButton = styled(Button)`
+  margin-top: 0;
 `
 
 export default ({ header, title, children, submitButtonTitle, onClickSubmit, formId, error }) => (
@@ -51,22 +49,25 @@ export default ({ header, title, children, submitButtonTitle, onClickSubmit, for
             { children }
           </Row>
         </Column>
-        <CtaButtonContainer lg={{ span: 4, offset: 3 }} md={{ span: 4, offset: 1 }} sm={{ span: 4 }}>
-          <Row>
-            {error &&
-              <InlineNotification
-                kind="error"
-                icondescription="Dismiss notification"
-                subtitle={error}
-                title="Oops!"
-              />
-            }
-            {submitButtonTitle && (
-              <CtaButton type="submit" id={formId}>{submitButtonTitle}</CtaButton>
-            )}
-          </Row>
-        </CtaButtonContainer>
       </Form>
     </ContentContainer>
+    <CtaButtonContainer lg={{ span: 4, offset: 3 }} md={{ span: 4, offset: 1 }} sm={{ span: 4 }}>
+      <Row>
+        {error && (
+          <InlineNotification
+            hideCloseButton
+            kind="error"
+            icondescription="Dismiss notification"
+            subtitle={error}
+            title="Oops!"
+          />
+        )}
+        {submitButtonTitle && (
+          <CtaButton type="submit" id={formId}>
+            {submitButtonTitle}
+          </CtaButton>
+        )}
+      </Row>
+    </CtaButtonContainer>
   </>
 )
