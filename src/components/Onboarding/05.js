0929ffe4c5d00d082b77d9c3b3ed251e7ea62ce4
Charles Duong <charlesduong14@gmail.com>
2020-07-20 17:41:09 -0500

    HVAMB-165: BlockPower quiz page update

--- src/components/Onboarding/05.js
+++ src/components/Onboarding/05.js
@@ -12,17 +12,30 @@ export default () => {
       <p>
         Voting is not just about electing a President. We elect hundreds of
         other government officials who control the polices that affect our
-        lives, including —
+        lives, including:
       </p>
-      <FigureContainer>
-        <FigureRow bgLevel={70}>Making housing more affordable</FigureRow>
-        <FigureRow bgLevel={60}>Lowering the cost of healthcare</FigureRow>
-        <FigureRow bgLevel={50}>Reducing racism</FigureRow>
-        <FigureRow bgLevel={40}>Improving water and air quality</FigureRow>
-        <FigureRow bgLevel={30}>Making college more affordable</FigureRow>
-        <FigureRow bgLevel={20}>Creating more higher paying jobs</FigureRow>
-        <FigureRow bgLevel={10}>Making ends meet with one job</FigureRow>
-      </FigureContainer>
+      <figure>
+        <FigureContainer>
+          <FigureRow bgLevel={70}>Making housing more affordable</FigureRow>
+          <FigureRow bgLevel={60}>Lowering the cost of healthcare</FigureRow>
+          <FigureRow bgLevel={50}>Reducing racism</FigureRow>
+          <FigureRow bgLevel={40}>Improving water and air quality</FigureRow>
+          <FigureRow bgLevel={30}>Making college more affordable</FigureRow>
+          <FigureRow bgLevel={20}>Creating more higher paying jobs</FigureRow>
+          <FigureRow bgLevel={10}>Making ends meet with one job</FigureRow>
+        </FigureContainer>
+        <br />
+        <figcaption>
+          *This list was sourced from
+          <br />
+          <a
+            href="https://www.thirdway.org/memo/a-nuanced-picture-of-what-black-americans-want-in-2020"
+            target="_blank"
+          >
+            https://www.thirdway.org/memo/a-nuanced-picture-of-what-black-americans-want-in-2020
+          </a>
+        </figcaption>
+      </figure>
     </TrainingLayout>
   );
 }
