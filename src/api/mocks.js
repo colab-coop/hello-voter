eef5b1a7156f8d6c14878cb3f9af02c0a54ce28b
Kevin Cooper <cooperka92@gmail.com>
2020-09-20 17:25:30 -0700

    Allow enabling mock data in local dev

--- src/api/mocks.js
+++ src/api/mocks.js
@@ -2,10 +2,40 @@ import { setupWorker, rest } from 'msw'
 import { AMBASSADOR_URL, PAYMENT_HISTORY_URL } from '../constants';
 
 export const MOCK_USER = {
-  payout_provider: "stripe",
-  account: {
-    account_data: {
-      last4: "1234",
+  "id": "fake123",
+  "external_id": "google:123",
+  "date_of_birth": "1/1/1990",
+  "first_name": "BP",
+  "last_name": "Tester",
+  "phone": "13135551212",
+  "email": "test@example.com",
+  "location": {
+    "srid": {
+      "low": 4326,
+      "high": 0
+    },
+    "x": -84.261336,
+    "y": 30.442406
+  },
+  "signup_completed": true,
+  "onboarding_completed": true,
+  "approved": true,
+  "locked": false,
+  "payout_provider": "stripe",
+  "payout_additional_data": null,
+  "admin": false,
+  "address": {
+    "address1": "1300 E Park Ave",
+    "state": "FL",
+    "zip": "32301",
+    "city": "Tallahassee"
+  },
+  "display_address": "1300 E Park Ave, Tallahassee, FL, 32301",
+  "display_name": "BP Tester",
+  "quiz_results": null,
+  "account": {
+    "account_data": {
+      "last4": "1234",
     },
   },
 }
