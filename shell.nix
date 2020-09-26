4e97225baeea365f3c58370bcc7af9eb7da7de7b
Ethan Winn <ethan@colab.coop>
2020-09-26 15:44:56 -0400

    Fixed error in nix shell file

--- shell.nix
+++ shell.nix
@@ -3,9 +3,9 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "hello-voter-dev";
 
-  buildInputs = [steam-run]
+  buildInputs = [steam-run];
 
   shellHook = ''
-      alias co="steam-run cypress open"
+      alias co="steam-run ./node_modules/.bin/cypress open"
     '';
 }
