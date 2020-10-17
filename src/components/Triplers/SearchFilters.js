fa34e5e4182951e6895b0c9f0ee1e0a5110d51b0
Kevin Cooper <cooperka92@gmail.com>
2020-10-17 15:00:15 -0700

    Use an Accordion with cleaner styles

--- src/components/Triplers/SearchFilters.js
+++ src/components/Triplers/SearchFilters.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Button, Form, Search, Slider, Dropdown } from 'carbon-components-react';
+import { Button, Form, Search, Slider, Dropdown, Accordion, AccordionItem } from 'carbon-components-react';
 import { breakpoints, spacing } from '../../theme';
 import { track } from '../../analytics';
 import './searchFilters.scss';
@@ -51,7 +51,7 @@ const MSA_OPTIONS = [
   "Mansfield, OH area",
 ];
 
-const SearchBarContainer = styled(Form)`
+const SearchFilterGrid = styled.div`
   display: grid;
   grid-auto-columns: 1fr;
   grid-column-gap: ${spacing[5]};
@@ -65,10 +65,7 @@ const SearchBarContainer = styled(Form)`
 `;
 
 const SearchFieldStyled = styled(Search)`
-  grid-column-end: span 5;
-  @media (max-width: ${breakpoints.md.width}) {
-    grid-column-end: span 6;
-  }
+  grid-column-end: span 6;
 `;
 
 const SearchButtonStyled = styled(Button)`
@@ -82,87 +79,97 @@ const SearchButtonStyled = styled(Button)`
 
 export const SearchFilters = ({ search, loading, searchInputs, onSearchInputChange }) => {
   return (
-    <SearchBarContainer
+    <Form
       onSubmit={(e) => {
         e.preventDefault();
         search();
       }}
     >
-      <SearchFieldStyled
-        name="firstName"
-        placeHolderText="First Name"
-        size="lg"
-        onChange={onSearchInputChange("firstName")}
-        value={searchInputs.firstName}
-        labelText=""
-      />
-      <SearchFieldStyled
-        name="lastName"
-        placeHolderText="Last Name"
-        size="lg"
-        onChange={onSearchInputChange("lastName")}
-        value={searchInputs.lastName}
-        labelText=""
-      />
-      <SearchFieldStyled
-        name="phone"
-        placeHolderText="Mobile Phone Number"
-        size="lg"
-        onChange={onSearchInputChange("phone")}
-        value={searchInputs.phone}
-        labelText=""
-      />
-      <Slider
-        name="distance"
-        labelText="Distance"
-        max={1}
-        min={0}
-        step={0.05}
-        onChange={onSearchInputChange("distance")}
-        value={searchInputs.distance}
-      />
-      <Dropdown
-        id="age"
-        name="age"
-        items={AGE_OPTIONS}
-        label="Choose an option"
-        titleText="Age"
-        onChange={onSearchInputChange("age")}
-        selectedItem={searchInputs.age}
-      />
-      <Dropdown
-        id="gender"
-        name="gender"
-        items={GENDER_OPTIONS}
-        label="Choose an option"
-        titleText="Gender"
-        onChange={onSearchInputChange("gender")}
-        selectedItem={searchInputs.gender}
-      />
-      <Dropdown
-        id="msa"
-        name="msa"
-        items={MSA_OPTIONS}
-        label="Choose an option"
-        titleText="Metro area"
-        onChange={onSearchInputChange("msa")}
-        selectedItem={searchInputs.msa}
-      />
+      <SearchFilterGrid>
+        <SearchFieldStyled
+          name="firstName"
+          placeHolderText="First Name"
+          size="lg"
+          onChange={onSearchInputChange("firstName")}
+          value={searchInputs.firstName}
+          labelText=""
+        />
+        <SearchFieldStyled
+          name="lastName"
+          placeHolderText="Last Name"
+          size="lg"
+          onChange={onSearchInputChange("lastName")}
+          value={searchInputs.lastName}
+          labelText=""
+        />
+      </SearchFilterGrid>
+      <Accordion>
+        <AccordionItem title="See more search filters">
+          <SearchFilterGrid style={{ marginTop: 0 }}>
+            <SearchFieldStyled
+              name="phone"
+              placeHolderText="Mobile Phone Number"
+              size="lg"
+              onChange={onSearchInputChange("phone")}
+              value={searchInputs.phone}
+              labelText=""
+            />
+            <Slider
+              name="distance"
+              labelText="Distance"
+              max={1}
+              min={0}
+              step={0.05}
+              onChange={onSearchInputChange("distance")}
+              value={searchInputs.distance}
+            />
+            <Dropdown
+              id="age"
+              name="age"
+              items={AGE_OPTIONS}
+              label="Choose an option"
+              titleText="Age"
+              onChange={onSearchInputChange("age")}
+              selectedItem={searchInputs.age}
+            />
+            <Dropdown
+              id="gender"
+              name="gender"
+              items={GENDER_OPTIONS}
+              label="Choose an option"
+              titleText="Gender"
+              onChange={onSearchInputChange("gender")}
+              selectedItem={searchInputs.gender}
+            />
+            <Dropdown
+              id="msa"
+              name="msa"
+              items={MSA_OPTIONS}
+              label="Choose an option"
+              titleText="Metro area"
+              onChange={onSearchInputChange("msa")}
+              selectedItem={searchInputs.msa}
+            />
+          </SearchFilterGrid>
+        </AccordionItem>
+      </Accordion>
 
-      <SearchButtonStyled
-        size="field"
-        kind="tertiary"
-        type="submit"
-        onClick={() => {
-          track({
-            action: "SearchTripler",
-            label: "Triplers/add",
-          });
-        }}
-        disabled={loading}
-      >
-        Search
-      </SearchButtonStyled>
-    </SearchBarContainer>
+      <SearchFilterGrid>
+        <SearchButtonStyled
+          size="field"
+          kind="tertiary"
+          type="submit"
+          onClick={() => {
+            track({
+              action: "SearchTripler",
+              label: "Triplers/add",
+            });
+          }}
+          disabled={loading}
+        >
+          Search
+        </SearchButtonStyled>
+      </SearchFilterGrid>
+    </Form>
   )
 };
