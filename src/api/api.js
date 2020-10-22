4d62f462ed40bc868479ad193ac842e8683e3d08
azrap <azraap@gmail.com>
2020-10-22 15:04:26 -0700

    DOB works

--- src/api/api.js
+++ src/api/api.js
@@ -165,6 +165,8 @@ export const saveProfile = async (user) => {
 }
 
 export const confirmTriplers = async (triplerId, json) => {
+
+  console.log(json)
   
   try {
     let res = await fetch(`${CONFIRM_TRIPLER_URL}/${triplerId}/start-confirm`, {
