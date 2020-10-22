9d2aa412b200609b0ead4577aee172b3e325f947
azrap <azraap@gmail.com>
2020-10-22 15:07:24 -0700

    remove console.log

--- src/api/api.js
+++ src/api/api.js
@@ -166,7 +166,7 @@ export const saveProfile = async (user) => {
 
 export const confirmTriplers = async (triplerId, json) => {
 
-  console.log(json)
+  
   
   try {
     let res = await fetch(`${CONFIRM_TRIPLER_URL}/${triplerId}/start-confirm`, {
