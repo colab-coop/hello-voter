dfd188ce59bc8b713d0c8e3de7b84fe5532e5b86
looshi <davluzius@gmail.com>
2020-10-31 10:44:19 -0600

    Copy edits for DeniedPage and certify checkbox

--- src/components/Profile/ContactInfoPage.js
+++ src/components/Profile/ContactInfoPage.js
@@ -158,8 +158,27 @@ export const ContactInfoPage = ({ ambassador, setAmbassador, err }) => {
 
 export const DeniedPage = () => {
   return (
-  <PageLayout title="Your account is under review.">
-    <p>Please contact support@blockpower.vote with any questions</p>
+  <PageLayout title="Thanks for being a BlockPower Ambassador!">
+    <p>
+    Thanks for being a BlockPower Ambassador! With your help, we have reached over 100,000 low-propensity Black voters so they can make their voices heard at the ballot box. 
+    </p>
+
+    <p>
+    Most of you should have received payment for your confirmed Triplers or heard from us about connecting to a bank account so outstanding payments can be processed. Some accounts are under review for potentially providing false information; those confirmed to have done that will not be paid out.
+    </p>
+
+    <p>
+    We are now focused on get-out-the-vote efforts, so you will not be able to access the BlockPower app.  Please continue to help by reaching out to your Triplers and asking them to encourage their Triplees to vote. 
+    </p>
+
+    <p>
+    We're grateful you joined us in making this model a success. You've been patient as we've made upgrades and have spread the word about BlockPower. We will build on your efforts and ideas to make BlockPower better and more responsive in the future.  Thanks again. 
+    </p>
+
+    <p>
+    Onward!<br/>
+    The BlockPower Team
+    </p>
   </PageLayout>
   )
 }
