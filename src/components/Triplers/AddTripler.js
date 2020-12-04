44c3b7bb768cb9a749805367733be16faa10a376
Ka-Ping Yee <zestyping@gmail.com>
2020-12-04 10:56:38 -0800

    Tidy up stray commas in Tripler addresses.

--- src/components/Triplers/AddTripler.js
+++ src/components/Triplers/AddTripler.js
@@ -11,24 +11,26 @@ import { useHistory } from "react-router-dom";
 import Loading from "../Loading";
 import { SearchFilters } from './SearchFilters';
 
-// Trim off any leading and trailing numbers, e.g. "123 Main St Apt 4" => "Main St".
-export function normalizeStreetName({ address1, address2, city, state, zip }) {
+// Gets the normalized first line of an address.
+export function normalizeAddress1(address) {
+  const { address1, address2, city, state, zip } = address || {};
   try {
     const { addressLine1 } = addresser.parseAddress(`${address1 || ''} ${address2 || ''}, ${city || ''}, ${state || ''} ${zip || ''}`);
     return addressLine1;
   } catch (e) {
-    // Just don't show it if we can't determine what their street name is.
+    // Just don't show it if we can't determine the first line of the address.
     return "";
   }
 }
 
 export function normalizeTripler(tripler) {
   const age = `(Age: ${tripler.age_decade || 'unknown'})`;
-  const streetName = normalizeStreetName(tripler.address || {});
+  const address1 = normalizeAddress1(tripler.address);
+  const { city, state } = tripler.address || {};
   return {
     id: tripler.id,
     name: `${tripler.first_name} ${tripler.last_name} ${age}`,
-    address: `${streetName} ${tripler.address?.city}, ${tripler.address?.state}`,
+    address: [address1, city, state].filter(x => x).join(', ')
   };
 }
 
